50b08b3bb34ab8426a6b6f15c0703b2c5a621eec
Maurizio Cingolani <mauriziocingolani74@gmail.com>
2016-07-06 16:20:15 +0200

    Lista posts

--- classes/Post.php
+++ classes/Post.php
@@ -1,6 +1,7 @@
 <?php
 
 class Post {
+
     public $PostID;
     public $Creato;
     public $Modificato;
@@ -8,8 +9,39 @@ class Post {
     public $Titolo;
     public $Contenuto;
     public $Utente; # oggetto di classe Utente corrispondente
-    
+    private static $nome_tabella = 'posts';
+
     public static function GetAll(Database $db) {
-        
+        try {
+            $query = "SELECT * FROM " . self::$nome_tabella . " " .
+                    "ORDER BY Creato DESC";
+            $posts = $db->executeQuery($query);
+            $data = array();
+            foreach ($posts as $post) :
+                $p = new Post;
+                foreach ($post as $prop => $valore) :
+                    $p->$prop = $valore;
+                endforeach;
+                $data[] = $p;
+            endforeach;
+            return $data;
+        } catch (Exception $e) {
+            return $e->getMessage() . ' (' . $e->getCode() . ')';
+        }
     }
+
+    public static function CreaNuovoPost(Database $db, array $parametri) {
+        try {
+            $parametri['titolo'] = str_replace("'", "''", $parametri['titolo']);
+            $parametri['contenuto'] = str_replace("'", "''", $parametri['contenuto']);
+            $query = "INSERT INTO " . self::$nome_tabella .
+                    "(Creato,UtenteID,Titolo,Contenuto) VALUES " .
+                    "(NOW(),{$parametri['utenteid']},'{$parametri['titolo']}','{$parametri['contenuto']}')";
+            $db->executeQuery($query);
+            return true;
+        } catch (Exception $e) {
+            return $e->getMessage() . ' (' . $e->getCode() . ')';
+        }
+    }
+
 }
